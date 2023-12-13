311c178620232af51b01d6a375b00de30ba03ce9
Tal Charnes <tal32123@gmail.com>
2023-12-13 18:43:01 +0200

    feat: fix routes

--- apps/mono-svc/src/app.controller.ts
+++ apps/mono-svc/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FilfilachumaService } from '@app/filfilachuma';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly filfilSvc: FilfilachumaService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('filfil')
+  getFilfilachuma(): string {
+    return this.filfilSvc.getFilfil();
   }
 }
--- apps/mono-svc/src/app.module.ts
+++ apps/mono-svc/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FilfilachumaModule } from '@app/filfilachuma';
 
 @Module({
-  imports: [],
+  imports: [FilfilachumaModule],
   controllers: [AppController],
   providers: [AppService],
 })
